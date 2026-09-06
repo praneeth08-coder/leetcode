add745e42fba0383484049693e23c4899aa043b4
praneeth <pss.praneeth2008@gmail.com>
2026-09-06 21:10:05 +0530

    Time: 647 ms (7.34%) | Memory: 44.7 MB (59.48%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -4,7 +4,7 @@ public:
         int a = nums.size();
         int c = 0;
         for (int i = 0; i < a; i++) {
-            long long prod = 1; // Use long long to prevent integer overflow
+            long long prod = 1; 
             for (int j = i; j < a; j++) {
                 prod = prod * nums[j];
                 if (prod < k) {
